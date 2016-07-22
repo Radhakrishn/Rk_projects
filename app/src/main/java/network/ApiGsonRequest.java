cb7842c9972470d3fba4bf1f3eada407589de834
Radhakrishna NP <radhakrishna.np@robosoftin.com>
2016-07-22 12:20:17 +0530

    gcm added , sign up updates

--- app/src/main/java/network/ApiGsonRequest.java
+++ app/src/main/java/network/ApiGsonRequest.java
@@ -53,7 +53,6 @@ public class ApiGsonRequest extends Request<IJRDataModel> {
 
     public ApiGsonRequest(MethodType methodType, String url, Listener<IJRDataModel> listener, ErrorListener errorListener, IJRDataModel model, Map<String, String> headers) {
         super(getVolleyMethodType(methodType), url, errorListener);
-        System.out.println("---------ApiGsonRequest-------------");
         mListener = listener;
         mDataModel = model;
         mGson = new Gson();
